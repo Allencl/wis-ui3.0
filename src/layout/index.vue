129af18ef12b504d3abf56759eecdae2efa1c245
Allencl <Allen__cc@163.com>
2022-03-02 17:34:59 +0800

    1

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,41 +2,8 @@
   <a-layout style="min-height: 100vh">
     <a-layout-sider v-model:collapsed="collapsed" collapsible>
       <div class="logo" />
-      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
-        <a-menu-item key="1">
-          <pie-chart-outlined />
-          <span>Option 1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <desktop-outlined />
-          <span>Option 2</span>
-        </a-menu-item>
-        <a-sub-menu key="sub1">
-          <template #title>
-            <span>
-              <user-outlined />
-              <span>User</span>
-            </span>
-          </template>
-          <a-menu-item key="3">Tom</a-menu-item>
-          <a-menu-item key="4">Bill</a-menu-item>
-          <a-menu-item key="5">Alex</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-            <span>
-              <team-outlined />
-              <span>Team</span>
-            </span>
-          </template>
-          <a-menu-item key="6">Team 1</a-menu-item>
-          <a-menu-item key="8">Team 2</a-menu-item>
-        </a-sub-menu>
-        <a-menu-item key="9">
-          <file-outlined />
-          <span>File</span>
-        </a-menu-item>
-      </a-menu>
+
+      <menuHTML />
     </a-layout-sider>
     <a-layout>
       <a-layout-header style="background: #fff; padding: 0" />
@@ -57,12 +24,12 @@
 </template>
 
 <script lang="ts" setup>
-  import { PieChartOutlined, DesktopOutlined, UserOutlined, TeamOutlined, FileOutlined } from '@ant-design/icons-vue';
+  import menuHTML from "./menu.vue";
   import { defineComponent, ref } from 'vue';
 
 
   const  collapsed= ref(false)
-  const  selectedKeys= ref(['1'])
+
 </script>
 
 
