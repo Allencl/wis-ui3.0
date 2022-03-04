57229064ae3bfef4eef2e32673f9248906da141a
Allencl <Allen__cc@163.com>
2022-03-04 15:45:59 +0800

    11

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,13 @@ import RoutersJSON from './router'
 
 const routes: Array<RouteRecordRaw> = [
   {
+    path: '/demo-list',
+    name: 'demolist',
+    component: () => import('@/views-demo/list/index.vue')
+  },
+
+
+  {
     path: '/',
     name: 'home',
     component: () => import('@/layout/home.vue')
