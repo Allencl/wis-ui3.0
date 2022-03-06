e3eb5d3e7645c289d6b7840c5c052eba9f7bc65d
Allencl <Allen__cc@163.com>
2022-03-06 23:19:37 +0800

    1

--- src/api/company.ts
+++ src/api/company.ts
@@ -8,6 +8,7 @@ import {httpHandle} from '@/http/http'
 // 获取 公司列表
 export const companyList= async(option:any)=>{
     return await httpHandle({
+        url:'company/index.do',
         method:'GET',
     })
 }
